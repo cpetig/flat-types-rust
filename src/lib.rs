649bc1b81d32914be38b9d6682139cb352689ee7
Christof Petig <christof.petig@arcor.de>
2024-10-05 11:06:40 +0200

    initial writing support

--- src/lib.rs
+++ src/lib.rs
@@ -37,27 +37,41 @@ impl<'a, T: Copy> View<'a, T> {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
 enum Error {
-    TooSmall,
+    /// overflow
+    BufferTooSmall,
+    /// buffer is already in use
+    BufferBusy,
+    /// invalid input values (validation)
+    InvalidValue,
 }
 
-trait ReadIndex {
-    //+ Into<usize> {
+trait IndexType {
     fn read(buffer: &[u8]) -> usize;
-    fn write(buffer: &[u8], value: usize) -> Result<usize, Error>;
+    fn write(buffer: &mut [u8], value: usize) -> Result<usize, Error>;
 }
 
-impl ReadIndex for u8 {
+impl IndexType for u8 {
     fn read(buffer: &[u8]) -> usize {
         buffer[0].into()
     }
 
-    fn write(buffer: &[u8], value: usize) -> Result<usize, Error> {
-        todo!()
+    fn write(buffer: &mut [u8], value: usize) -> Result<usize, Error> {
+        let Ok(value) = u8::try_from(value) else {
+            return Err(Error::InvalidValue);
+        };
+        match buffer.get_mut(0) {
+            Some(b) => {
+                *b = value;
+                Ok(std::mem::size_of::<Self>())
+            }
+            None => Err(Error::BufferTooSmall),
+        }
     }
 }
 
-impl<'a, IDX: ReadIndex + Copy> Debug for View<'a, String<IDX>> {
+impl<'a, IDX: IndexType + Copy> Debug for View<'a, String<IDX>> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let start = IDX::read(self.buffer);
         let len = IDX::read(&self.buffer[std::mem::size_of::<IDX>()..]);
@@ -69,7 +83,7 @@ impl<'a, IDX: ReadIndex + Copy> Debug for View<'a, String<IDX>> {
     }
 }
 
-impl<'a, T: Copy, IDX: ReadIndex + Copy> Debug for View<'a, Vec<T, IDX>>
+impl<'a, T: Copy, IDX: IndexType + Copy> Debug for View<'a, Vec<T, IDX>>
 where
     View<'a, T>: Debug,
 {
@@ -100,27 +114,48 @@ impl<'a> Context<'a> {
 struct Writer<'a, T> {
     buffer: &'a mut [u8],
     phantom: PhantomData<T>,
+    current_end: usize,
 }
 
-trait Assign<T> {
-    fn set(self, value: T);
+trait Assign<'a, T, U: Copy> {
+    fn set(self, value: T) -> Result<View<'a, U>, Error>;
 }
 
-trait Assignable<T> {
-    fn set(obj: &mut T, value: Self);
-}
+// trait Assignable<T> {
+//     fn set(obj: &mut T, value: Self);
+// }
 
 impl<'a, T: Copy> Writer<'a, T> {
-    // type Context=Context<'a>;
-
     pub fn new(ctx: Context<'a>) -> Self {
-        todo!()
+        Self {
+            buffer: ctx.buffer,
+            phantom: PhantomData,
+            current_end: 0,
+        }
     }
 }
 
 // impl<'a, T: Copy, U: Assignable<T>> Writer<'a,T> {
 //         pub fn set(&mut self, value: U) -> View<'a, T> { todo!()}
 // }
+
+impl<'a, IDX: IndexType + Copy> Assign<'a, &str, String<IDX>> for Writer<'a, String<IDX>> {
+    fn set(mut self, value: &str) -> Result<View<'a, String<IDX>>, Error> {
+        if self.current_end != 0 {
+            return Err(Error::BufferBusy);
+        }
+        let data_pos = 2 * core::mem::size_of::<IDX>();
+        let len = value.len();
+        self.current_end += data_pos + len;
+        IDX::write(self.buffer, data_pos)?;
+        IDX::write(&mut self.buffer[core::mem::size_of::<IDX>()..], len)?;
+        self.buffer[data_pos..data_pos + len].copy_from_slice(value.as_bytes());
+        Ok(View {
+            buffer: &self.buffer[0..self.current_end],
+            phantom: PhantomData,
+        })
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -148,7 +183,7 @@ mod tests {
         let mut writebuffer = [0u8; 256];
         let ctx = Context::new(&mut writebuffer);
         let writer = Writer::<String<u8>>::new(ctx);
-        // let view = writer.set("test");
-        // assert_eq!(format("{view:?}"), "test");
+        let view = writer.set("test").expect("write ok");
+        assert_eq!(format!("{view:?}"), "test");
     }
 }
